462952ad9d6948414e1ef8877c2e2c835c247ed9
JoshieBoshie <joshua91214@berkeley.edu>
2018-03-22 10:09:43 -0700

    weird, trying to figure out a way to route the form to the pokemon's create method

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,4 +1,8 @@
 class PokemonsController < ApplicationController
+  def new
+    @pokemon = Pokemon.new
+  end
+
   def capture
     @pokemon = Pokemon.find(params[:id])
     @pokemon.trainer_id = current_trainer.id
@@ -14,5 +18,17 @@ class PokemonsController < ApplicationController
       @pokemon.save
     end
     redirect_to trainer_path(current_trainer.id)
+  end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+    @pokemon.health = 100
+    @pokemon.level = 1
+    @pokemon.trainer_id = current_trainer.id
+    redirect_to trainer_path(current_trainer.id)
+  end
+
+  def pokemon_params
+    params.require(:pokemon).permit(:name,:ndex)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :trainers
   patch "pokemon/capture", to: "pokemons#capture", as: "capture"
   patch "pokemon/damage", to: "pokemons#damage", as: "damage"
+  get 'pokemon/new', to: 'pokemons#new'
+  post 'pokemon/trainers', to: 'pokemons#create'
 end
