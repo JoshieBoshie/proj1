e0f3612e0e439e80fc0a9a33bfbdbaf379fb38f5
JoshieBoshie <joshua91214@berkeley.edu>
2018-03-22 09:11:15 -0700

    parts 1 through 4 done. Didn't really commit for the sake of the text editor's user friendliness. Going to commit a lot now

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,18 @@
+class PokemonsController < ApplicationController
+  def capture
+    @pokemon = Pokemon.find(params[:id])
+    @pokemon.trainer_id = current_trainer.id
+    @pokemon.save
+    redirect_to root_path
+  end
+  def damage
+    @pokemon = Pokemon.find(params[:id])
+    @pokemon.health -= 10
+    if @pokemon.health<=0
+      @pokemon.destroy
+    else
+      @pokemon.save
+    end
+    redirect_to trainer_path(current_trainer.id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :trainers
   root to: "home#index"
   resources :trainers
+  patch "pokemon/capture", to: "pokemons#capture", as: "capture"
+  patch "pokemon/damage", to: "pokemons#damage", as: "damage"
 end
